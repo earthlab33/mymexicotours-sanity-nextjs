efb3a07a876c78f7c74678c041713361770645dd
Trey Donovan <trey@powerhat.com>
2023-10-09 10:05:43 -0600

    updated vistas page with EcoBuild page code, to remedy thumbs not loading.

--- app/(site)/components/Vista.tsx
+++ app/(site)/components/Vista.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 interface VistaFields {
   title: string;
-  thumb: {
+  images: {
     thumbnails: {
       large: {
         url: string;
@@ -21,19 +21,27 @@ interface VistaProps {
 }
 
 export default function Vista({ vista }: VistaProps) {
+  // Check if image exists and has the expected structure
+  const imageUrl = vista.fields.images && vista.fields.images[0] && vista.fields.images[0].thumbnails && vista.fields.images[0].thumbnails.large && vista.fields.images[0].thumbnails.large.url;
+
   return (
-    <Link href={`/vista/${vista.id}`} passHref className="relative block w-full h-48 group cover fill"> 
+    <Link href={`/vista/${vista.id}`} passHref className="relative block w-full h-60 group cover fill"> 
+      {/* Render the Image component only if imageUrl exists */}
+      {imageUrl && (
         <Image 
-          src={vista.fields.thumb[0].thumbnails.large.url} 
+          src={imageUrl} 
+          className="w-full h-auto"
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           alt={vista.fields.title}
         />
-        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"> {/* Title that appears on hover */}
-          <span className="text-white bg-black bg-opacity-50 p-2 rounded">
-            {vista.fields.title}
-          </span>
-        </div>
+      )}
+
+      <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"> 
+        <span className="text-white bg-black bg-opacity-50 p-2 rounded">
+          {vista.fields.title}
+        </span>
+      </div>
     </Link>
   );
 }
